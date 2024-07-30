1ce353034cc822d9fa25bc64b2fecd6b2518f04f
jyedev04 <jyedev04@gmail.com>
2024-07-31 03:43:46 +0900

    fastlms 관리자 로그인(완)2

--- src/main/java/com/zerobase/fastlms/admin/AdminMainController.java
+++ src/main/java/com/zerobase/fastlms/admin/AdminMainController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class AdminMainController {
     @GetMapping("/admin/main")
     public String main(){
+
         return "admin/main";
     }
 }
